4b2ddaca331f604dc8bd51fba7665b10df0b04f1
Kokoroou <66433773+Kokoroou@users.noreply.github.com>
2021-11-02 08:05:18 +0700

    Change file in branch main

--- src/manga/Main.java
+++ src/manga/Main.java
@@ -3,7 +3,7 @@ package manga;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Manga World!");
+		System.out.println("Hello Manga World_main!");
 
 	}
 
